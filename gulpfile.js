ae16c1955b6afb94655e746fa6c0eff9a0f7fd36
Algram <aliasgram@gmail.com>
2016-05-09 21:50:03 +0200

    Updated -gitignore and added iamgemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp  = require('gulp');
 var folders = require('gulp-folders');
+var imagemin = require('gulp-imagemin');
 var path = require('path');
 var pathToFolder = 'base';
 
@@ -8,12 +9,14 @@ gulp.task('sometask', function() {
 });
 
 gulp.task('task', folders(pathToFolder, function(folder){
-	//This will loop over all folders inside pathToFolder main, secondary
-	//Return stream so gulp-folders can concatenate all of them
-	//so you still can use safely use gulp multitasking
+  //This will loop over all folders inside pathToFolder main, secondary
+  //Return stream so gulp-folders can concatenate all of them
+  //so you still can use safely use gulp multitasking
+  console.log(folder);
 
   return gulp.src(path.join(pathToFolder, folder, '*.jpg'))
-		.pipe(console.log(folder + '.jpg'));
+    .pipe(imagemin({optimizationLevel: 5}))
+    .pipe(gulp.dest('build/img'));
 }));
 
 gulp.task('watch', function() {
